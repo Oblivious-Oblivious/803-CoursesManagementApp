751d05e3154b4a70c9d11892b2a9e9011bb865a4
Oblivious <thanos.papap42@gmail.com>
2022-04-13 06:54:51 +0300

    Update EntryPoint.java

--- client/cli/EntryPoint.java
+++ client/cli/EntryPoint.java
@@ -1,10 +1,7 @@
 package client.cli;
 
-import app.src.hello.Hello;
-
 public class EntryPoint {
     public static void main(String args[]) {
-        Hello h = new Hello();
-        System.out.println(h.random());
+        System.out.println("Hello, World!");
     }
 }

8288d24f4c248bf3eb073b76bb0145508200cfe9
Oblivious <thanos.papap42@gmail.com>
2022-04-23 14:40:31 +0300

    Added new objects to test.

--- app/test/TestRunner.java
+++ app/test/TestRunner.java
@@ -1,14 +1,19 @@
 package app.test;
 
 import jspec.*;
-import app.src.entities.*;
+
 import app.test.entities.*;
+import app.test.login.*;
 
 public class TestRunner {
     public static void main(String args[]) {
         SpecModule modules[] = {
             new CourseTest(),
+            new SchemaTest(),
             new StudentRegistrationTest(),
+            new ProfessorLoginTest(),
+            new ProfessorRegistrationTest(),
+            new RegistrationTokenTest(),
         };
         Spec s = new Spec(modules);
         s.run_spec_suite("all");
--- dbms/custom/test/TestRunner.java
+++ dbms/custom/test/TestRunner.java
@@ -1,14 +1,13 @@
 package dbms.custom.test;
 
 import jspec.*;
-
+import app.test.entities.SchemaTest;
 import dbms.custom.src.*;
 
 public class TestRunner {
     public static void main(String args[]) {
         SpecModule modules[] = {
             new CustomDBTest(),
-            new LoginTokenTest(),
         };
         Spec s = new Spec(modules);
         s.run_spec_suite("all");

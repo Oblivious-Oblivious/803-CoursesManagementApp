4c6504dd3538b77913fa500bc5f3346df803beda
Oblivious <thanos.papap42@gmail.com>
2022-04-24 18:38:07 +0300

    Implemented a course adder object.

---
+++ app/src/list_of_courses/Adder.java
@@ -0,0 +1,18 @@
+package app.src.list_of_courses;
+
+import app.src.entities.Course;
+import app.src.entities.DBMSGateway;
+import app.src.entities.Schema;
+
+public class Adder {
+    DBMSGateway courses_db = null;
+    
+    public Adder(DBMSGateway courses_db) {
+        this.courses_db = courses_db;
+    }
+
+    public void add_new_course(Course new_course) {
+        Schema new_schema = new Schema(new_course.id, new_course);
+        this.courses_db.save(new_schema);
+    }
+}
---
+++ app/test/list_of_courses/AdderTest.java
@@ -0,0 +1,79 @@
+package app.test.list_of_courses;
+
+import app.src.entities.Course;
+import app.src.entities.DBMSGateway;
+import app.src.entities.Schema;
+
+import app.src.list_of_courses.Adder;
+
+import dbms.CustomGatewayImplementation;
+
+import jspec.*;
+
+public class AdderTest extends SpecModule {
+    private DBMSGateway courses;
+    private Course anastasiadis;
+    private Course kavousianos;
+    private Course zarras;
+
+    public void spec_code() {
+        before_each(() -> {
+            this.kavousianos = new Course(
+                "c1",
+                "Digital Design I",
+                "Some info about ditital design 1",
+                "Kavousianos",
+                "2020",
+                "3"
+            );
+            this.zarras = new Course(
+                "c2",
+                "Software Engineering",
+                "More info about S. E.",
+                "Zarras",
+                "2022",
+                "8"
+            );
+            this.anastasiadis = new Course(
+                "c3",
+                "Operating Systems",
+                "OS class information and syllabus",
+                "Anastasiadis",
+                "2021",
+                "7"
+            );
+
+            this.courses = new CustomGatewayImplementation("Courses");
+            this.courses.save(new Schema("c1", kavousianos));
+            this.courses.save(new Schema("c2", zarras));
+            this.courses.save(new Schema("c3", anastasiadis));
+        });
+
+        describe("Adder object", () -> {
+            it("creates a new Adder object", () -> {
+                Adder ad = new Adder(this.courses);
+                assert_that(ad).isnot(null);
+            });
+
+            it("adds a new course in the course list", () -> {
+                Adder ad = new Adder(this.courses);
+                Course mamoulis = new Course(
+                    "c99",
+                    "Introduction to Programming",
+                    "Python introduction, iterations, conditionals, asasignments",
+                    "Mamoulis",
+                    "2021",
+                    "1"
+                );
+                ad.add_new_course(mamoulis);
+
+                assert_that(this.courses.get_all_items().size()).equals_to(4);
+                                
+                /* TODO Implement the null object pattern */
+                Schema last_item = this.courses.get_by_id("c99");
+                Course last_course = (Course)last_item.value();
+                assert_that(last_course.equals(mamoulis)).is(true);
+            });
+        });
+    }
+}

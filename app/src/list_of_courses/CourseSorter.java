21570f01508e8c7bdfe7c93b0527b762e5f22154
Oblivious <thanos.papap42@gmail.com>
2022-04-26 21:20:02 +0300

    Update CourseSorter.java

--- app/src/list_of_courses/CourseSorter.java
+++ app/src/list_of_courses/CourseSorter.java
@@ -6,6 +6,5 @@ import app.src.entities.DBMSGateway;
 import app.src.entities.Schema;
 
 public interface CourseSorter {
-    /* TODO Get null object pattern for sorter as well */
     public ArrayList<Schema> sort(DBMSGateway db);
 }

6694b0191074d3c7d01d63205fa831248d5135ed
Oblivious <thanos.papap42@gmail.com>
2022-05-08 20:14:55 +0300

    Combined tests.

--- persistence/TestRunner.java
+++ persistence/TestRunner.java
@@ -1,13 +1,16 @@
-package dbms.custom.test;
+package persistence;
 
 import jspec.*;
-import app.test.entities.SchemaTest;
-import dbms.custom.src.*;
+import persistence.custom.test.CustomDBTest;
+import persistence.dynamiclist.test.DynamicListTest;
+import persistence.sqlite.test.SqliteApiTest;
 
 public class TestRunner {
     public static void main(String args[]) {
         SpecModule modules[] = {
             new CustomDBTest(),
+            new DynamicListTest(),
+            new SqliteApiTest(),
         };
         Spec s = new Spec(modules);
         s.run_spec_suite("all");

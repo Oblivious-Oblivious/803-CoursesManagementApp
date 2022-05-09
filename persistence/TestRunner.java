c5a1a2e7c1d7b3fa945b6c71650b485e46eeb922
Oblivious <thanos.papap42@gmail.com>
2022-05-10 01:26:35 +0300

    Removed DynamicList implementation.

--- persistence/TestRunner.java
+++ persistence/TestRunner.java
@@ -2,14 +2,12 @@ package persistence;
 
 import jspec.*;
 import persistence.custom.test.CustomDBTest;
-import persistence.dynamiclist.test.DynamicListTest;
 import persistence.sqlite.test.SqliteApiTest;
 
 public class TestRunner {
     public static void main(String args[]) {
         SpecModule modules[] = {
             new CustomDBTest(),
-            new DynamicListTest(),
             new SqliteApiTest(),
         };
         Spec s = new Spec(modules);

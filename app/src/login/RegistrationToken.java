991ab69f615eb4c583e7aacaef4e4471c028354a
Oblivious <thanos.papap42@gmail.com>
2022-05-11 05:48:36 +0300

    Fixed registration token to be appropriate for DB insertion.

--- app/src/login/RegistrationToken.java
+++ app/src/login/RegistrationToken.java
@@ -1,9 +1,6 @@
 package app.src.login;
-// TODO Inlcude jar files in gitignore
-import java.util.ArrayList;
 
 import java.io.Serializable;
-import app.src.entities.Course;
 import app.src.entities.PersistenceGateway;
 import persistence.CustomGatewayImplementation;
 
@@ -13,14 +10,19 @@ public class RegistrationToken implements Serializable {
     public String password;
 
     /* TODO inject this */
-    public PersistenceGateway courses_db = new CustomGatewayImplementation("Accounts");
+    private transient PersistenceGateway courses_db = null;
 
     public RegistrationToken(String id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.courses_db = new CustomGatewayImplementation(this.username + "_courses");
     }
 
+    public PersistenceGateway get_courses_db() {
+        if(this.courses_db == null)
+            this.courses_db = new CustomGatewayImplementation(this.username + "_courses");
+        return this.courses_db;
     }
 
     public boolean equals(RegistrationToken other) {

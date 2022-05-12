7c11fe4a3540b4480fd01c25997685449114182f
Oblivious <thanos.papap42@gmail.com>
2022-05-12 12:53:20 +0300

    Create PercentileStatisticStrategy.java

---
+++ app/src/stats/PercentileStatisticStrategy.java
@@ -0,0 +1,25 @@
+package app.src.stats;
+
+import app.src.entities.Course;
+import app.src.entities.StatisticsStrategy;
+
+/* TODO Inject this */
+import statistics.ApacheMathDescriptiveStatisticsGatewayImplementation;
+
+public class PercentileStatisticStrategy extends StatisticsStrategy {
+    private ApacheMathDescriptiveStatisticsGatewayImplementation ds = null;
+
+    public PercentileStatisticStrategy(Course course) {
+        this.ds = new ApacheMathDescriptiveStatisticsGatewayImplementation(this.grades_to_double_array(course));
+    }
+
+    @Override
+    public double calculate() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Percentile";
+    }
+}

324ddb857d70c36e9075c4df4a0ecd24d513d5a9
Oblivious <thanos.papap42@gmail.com>
2022-05-15 15:43:43 +0300

    Added dummy index page.

--- client/web/boot_backend/src/main/java/boot_backend/BootBackendApplication.java
+++ client/web/boot_backend/src/main/java/boot_backend/BootBackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,39 +15,28 @@ public class BootBackendApplication {
         SpringApplication.run(BootBackendApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String root() {
-        /* TODO Implement html */
-        return "HOMEPAGE";
-    }
+    // @GetMapping("/register")
+    // public String register() {
+    //     return "REGISTER PAGE";
+    // }
 
-    @GetMapping("/register")
-    public String register() {
-        /* TODO Implement html */
-        return "REGISTER PAGE";
-    }
+    // @GetMapping("/login")
+    // public String login() {
+    //     return "LOGIN PAGE";
+    // }
 
-    @GetMapping("/login")
-    public String login() {
-        /* TODO Implement html */
-        return "LOGIN PAGE";
-    }
+    // @GetMapping("/courses")
+    // public String courses() {
+    //     return "COURSES PAGE";
+    // }
 
-    @GetMapping("/courses")
-    public String courses() {
-        /* TODO Implement html */
-        return "COURSES PAGE";
-    }
+    // @GetMapping("/course/{id}")
+    // public String course() {
+    //     return "COURSES PAGE";
+    // }
 
-    @GetMapping("/course/{id}")
-    public String course() {
-        /* TODO Implement html */
-        return "COURSES PAGE";
-    }
-
-    @GetMapping(value = "/course/{cid}/student/{sid}")
-    public String student(@PathVariable String cid, @PathVariable String sid) {
-        /* TODO Implement html */
-        return "(COURSE,STUDENT) : (" + cid + "," + sid + ")";
-    }
+    // @GetMapping(value = "/course/{cid}/student/{sid}")
+    // public String student(@PathVariable String cid, @PathVariable String sid) {
+    //     return "(COURSE,STUDENT) : (" + cid + "," + sid + ")";
+    // }
 }

903e27fed39c23b2bff9fabc8e406b78412fe221
Oblivious <thanos.papap42@gmail.com>
2022-05-17 00:13:01 +0300

    Update CourseTest.java

--- app/test/entities/CourseTest.java
+++ app/test/entities/CourseTest.java
@@ -9,18 +9,16 @@ public class CourseTest extends SpecModule {
     public void spec_code() {
         describe("Course object", () -> {
             before_each(() -> {
-                String id = "MYY402";
                 String name = "Electronics";
                 String syllabus = "Introduction to electronics. Amplifiers - Operational amplifiers. Semiconductor theory. The p-n junction - Diodes. Diode circuits (rectifier and limiting circuits). Field effect transistors and bipolar junction transistors: a) physical structure and operation, b) current-voltage characteristics, c) DC operation - bias, d) small-signal equivalent circuit models. Single-stage transistor amplifiers: biasing and operation. Differential amplifiers. Multi-stage amplifiers. Frequency response. Feedback.";
                 String instructor = "Tsiatoyhas";
                 String year = "2022";
                 String semester = "4";
                 
-                this.c = new Course(id, name, syllabus, instructor, year, semester);
+                this.c = new Course(name, syllabus, instructor, year, semester);
             });
 
             it("creates a random course object with passed data", () -> {
-                assert_that(c.id).equals_to("MYY402");
                 assert_that(c.name).equals_to("Electronics");
                 assert_that(c.syllabus).equals_to("Introduction to electronics. Amplifiers - Operational amplifiers. Semiconductor theory. The p-n junction - Diodes. Diode circuits (rectifier and limiting circuits). Field effect transistors and bipolar junction transistors: a) physical structure and operation, b) current-voltage characteristics, c) DC operation - bias, d) small-signal equivalent circuit models. Single-stage transistor amplifiers: biasing and operation. Differential amplifiers. Multi-stage amplifiers. Frequency response. Feedback.");
                 assert_that(c.instructor).equals_to("Tsiatoyhas");

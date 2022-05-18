a35682854e238a34c8028427569cc2dcc5107b03
Oblivious <thanos.papap42@gmail.com>
2022-05-18 12:24:03 +0300

    Fixed login controller, now checks for equality in registration tokens.

--- client/web/LoginController.java
+++ client/web/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import app.src.entities.Identifiable;
+import app.src.entities.PersistenceGateway;
 import app.src.entities.RegistrationToken;
 import app.src.boundaries.LoginBoundary;
 import app.src.login.ProfessorLogin;
@@ -16,10 +18,16 @@ public class LoginController {
     @PostMapping("/authenticate_credentials")
     public String authenticate_credentials(RegistrationToken reg) {
         RegistrationToken token = new RegistrationToken(reg.username, reg.password);
-        token.id = reg.id;
         LoginBoundary checker = new ProfessorLogin(token, new SqliteGatewayImplementation("Accounts"));
-        if(checker.login())
-            return "redirect:/courses?userid=" + token.id;
+        if(checker.login()) {
+            PersistenceGateway accounts_db = new SqliteGatewayImplementation("Accounts");
+            String current_id = "";
+            for(Identifiable account : accounts_db.get_all_items())
+                if(account.equals((RegistrationToken)token))
+                    current_id = account.id;
+
+            return "redirect:/courses?userid=" + current_id;
+        }
         return "redirect:/login";
         /* TODO Add a message dialog that rejects invalid inputs */
     }
